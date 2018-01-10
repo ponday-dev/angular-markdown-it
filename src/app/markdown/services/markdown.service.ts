c43d37c132d83e8340b210e15d737fc29c89cdbf
ponday-dev <ponday.dev+github@gmail.com>
2018-01-11 00:31:20 +0900

    [update] implement markdown.service

--- src/app/markdown/services/markdown.service.ts
+++ src/app/markdown/services/markdown.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import * as MarkdownIt from 'markdown-it';
+
 @Injectable()
 export class MarkdownService {
 
-  constructor() { }
+  private md: MarkdownIt.MarkdownIt;
+
+  constructor() {
+    this.md = MarkdownIt();
+  }
+
+  render(text: string): Observable<string>{
+    return fromPromise(this.renderPromise(text));
+  }
+
+  private renderPromise(text: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      try {
+        const result = this.md.render(text);
+        resolve(result);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 
 }
