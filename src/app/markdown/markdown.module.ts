5d1e63df986e3c258ef5002fe49bdcef159d4f10
ponday-dev <ponday.dev+github@gmail.com>
2018-01-11 00:32:22 +0900

    [update] makrdown.module

--- src/app/markdown/markdown.module.ts
+++ src/app/markdown/markdown.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MarkdownEditorComponent } from './components/markdown-editor/markdown-editor.component';
+import { MarkdownService } from './services/markdown.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: []
+  exports: [MarkdownEditorComponent],
+  declarations: [MarkdownEditorComponent],
+  providers: [MarkdownService]
 })
 export class MarkdownModule { }
